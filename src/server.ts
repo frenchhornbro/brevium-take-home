870366fdf64b36aecf1945851b5789f4254af69c
Hyrum Durfee <htdurfee@gmail.com>
2026-02-25 21:10:23 -0700

    add(main, server)

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+export default class Server {
+  public async run() {
+    console.log("Server is running...");
+    // TODO: Call endpoint
+    this.stop();
+  }
+
+  private async stop() {
+    // TODO: Call endpoint
+  }
+
+  public async getAppointmentRequest() {
+    console.log("Getting appointment request...");
+    // TODO
+  }
+}
